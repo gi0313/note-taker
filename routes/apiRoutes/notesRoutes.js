4e833ffde15b0c61e2db2c1827e5b131bd5332e6
Giovanni Claro <gio.claro316@gmail.com>
2022-06-20 19:43:19 -0400

    routes and stuff added

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,1 +1,17 @@
 const router = require('express').Router();
+const content = require('../../Develop/db/db.json');
+const {newNote} = require('../../lib/notes');
+const {nanoid} = require('nanoid')
+
+router.get('/notes', (req, res) => {
+    res.json(content);
+})
+
+router.post('/notes', (req, res) => {
+    req.body.id = nanoid();
+
+    const note = newNote(req.body, content);
+    req.json(note);
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 //npm i express -y
+//nanoID npm install --save nanoid
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
 app.use(express.urlencoded({extended: true}));
-
 app.use(express.json());
-
 app.use(express.static('public'));
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, ()=> {
+    console.log(`API server now on PORT ${PORT}`)
+})
